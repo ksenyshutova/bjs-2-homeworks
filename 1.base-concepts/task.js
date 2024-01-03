91e1da6d058e485c2dab04584fc1f2bc27b9c691
ksenyshutova <shutovasn@mail.ru>
2024-01-03 20:01:36 +0300

    ДЗ по основным понятиям

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,6 @@
 "use strict";
 
-solveEquation(1, 25, 8);
+solveEquation(1, 0, 8);
 calculateTotalMortgage(10, 0, 50000, 12);
 calculateTotalMortgage(10, 1000, 50000, 12);
 calculateTotalMortgage(10, 0, 20000, 24);
@@ -12,14 +12,11 @@ calculateTotalMortgage(15, 0, 10000, 36);
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = Math.pow(b, 2) - 4 * a * c;
-	if (d < 0) {
-		arr = [];
-	} else if (d === 0) {
+	if (d === 0) {
 		arr = [(-b / (2 * a))];
 	} else if (d > 0) {
 		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
 	}
-	console.log(arr);
 	return arr;
 }
 
@@ -28,6 +25,5 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let bodyCredit = amount - contribution;
 	let amountMonth = bodyCredit * (percentMonth + (percentMonth / ((Math.pow((1 + percentMonth), countMonths) - 1))));
 	let sum = amountMonth * countMonths;
-	console.log(sum.toFixed(2));
-	return (Number(sum.toFixed(2)));
+	return Number(sum.toFixed(2));
 }
