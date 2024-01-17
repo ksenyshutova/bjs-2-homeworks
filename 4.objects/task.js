a5ef66293eaac6ebbedbba535d18f65ad17fd0c6
ksenyshutova <shutovasn@mail.ru>
2024-01-17 23:43:06 +0300

    Домашнее задание по объектам

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty('marks') === false) {
+		console.log('Добавление невозможно.')
+	}
+	this.marks = marks;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	let average = 0;
+	if (this.marks.length === 0 || this.hasOwnProperty('marks') === false) {
+		return average;
+	}
+	this.marks.reduce((acc, item, index) => {
+		acc += item;
+		if (index === this.marks.length - 1) {
+			average = acc / this.marks.length;
+		}
+		return acc;
+	}, 0);
+	return average;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.exclude = reason;
 }
